f96ea7587c55bfd8a831d16d773a38a71a2b3d7e
Musa Ahmed <musaa.ahmed7@gmail.com>
2025-08-10 17:31:02 -0400

    query params finally work

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,72 @@
-import { useState } from 'react'
-import Joker from './components/joker'
+import { useState } from "react";
+import Joker from "./components/joker";
+import { useEffect } from "react";
 
 function App() {
-  const [jokerName, setJokerName] = useState('');
+  const [jokerInfo, setJokerInfo] = useState({
+    name: "Joker",
+    desc: "+4 Mult",
+    rarity: "Uncommon",
+    mainImage: "/sj2.webp",
+    overlay: "",
+    jokerTextDisabled: false,
+    isSmall: false,
+  });
+
+  const [ready, setReady] = useState(false);
+
+
+  useEffect(() => {
+    const url = new URL(window.location);
+    const info = url.searchParams.get("jokerInfo");
+    setJokerInfo(JSON.parse(info));
+    setReady(true);
+  }, []);
+
+  useEffect(() => {
+    if (ready == false) {
+      return
+    }
+    console.log(JSON.stringify(jokerInfo));
+    const url = new URL(window.location);
+    const jInfo = JSON.stringify(jokerInfo);
+    const sp = url.searchParams.get("jokerInfo");
+
+    // console.log(jInfo, sp)
+
+    if (sp !== jInfo) {
+      url.searchParams.set("jokerInfo", jInfo);
+      history.pushState(null, "", url);
+    }
+  }, [jokerInfo]);
 
   return (
-    <div className='body-text bg-[url(/bg.webp)] h-dvh w-full bg-cover bg-center bg-repeat-y flex flex-row items-center justify-center'>
+    <div className="body-text bg-[url(/bg.webp)] h-dvh w-full bg-cover bg-center bg-repeat-y flex flex-row items-center justify-center">
       <div>
-        <h1 className='text-9xl text-white body-text'>hello!</h1>
-        <input className='bg-white p-3' type='text' value={jokerName} onChange={e => setJokerName(e.target.value)}/>
+        <h1 className="text-9xl text-white body-text">hello!</h1>
+        <div>
+          <label>Name</label>
+          <input
+            className="bg-white p-3"
+            type="text"
+            value={jokerInfo.name}
+            onChange={(e) =>
+              setJokerInfo({
+                name: e.target.value,
+                desc: "+4 Mult",
+                rarity: "Uncommon",
+                mainImage: "/sj2.webp",
+                overlay: "",
+                jokerTextDisabled: false,
+                isSmall: false,
+              })
+            }
+          />
+        </div>
       </div>
-      <Joker isBig={true} name={"Joker"} desc={"+4 Mult"} rarity={"Common"} mainImage={"/sj2.webp"} overlay={""}/>
-    </div >
-  )
+      <Joker jokerInfo={jokerInfo} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/joker.jsx
+++ src/components/joker.jsx
@@ -1,23 +1,39 @@
-export default function Joker({ isBig, name, desc, rarity, overlay, mainImage, backgroundPath}) {
-  if (isBig) {
+import { useEffect } from "react";
+
+export default function Joker({ jokerInfo }) {
+
+    console.log(new URLSearchParams(jokerInfo))
+    console.log(jokerInfo.isSmall)
+
+  if (jokerInfo.isSmall == "false" || jokerInfo.isSmall == false) {
     return (
       <div className="flex flex-col gap-10">
         <div className="h-[25rem] w-[19rem] rounded flex flex-row p-3 pixel-corners bg-white relative">
-          <div className={`absolute top-0 left-0 h-full w-full z-10 rounded ${overlay}`}></div>
+          <div
+            className={`absolute top-0 left-0 h-full w-full z-10 rounded ${jokerInfo.overlay}`}
+          ></div>
           <div className="flex flex-row">
-            <JokerCardSideText isInverted={false} />
+            <JokerCardSideText
+              isInverted={false}
+              isDisabled={jokerInfo.jokerTextDisabled}
+            />
             <div className="flex items-center px-2">
-              <img src={`${mainImage}`} className="" />
+              <img src={`${jokerInfo.mainImage}`} className="" />
             </div>
-            <JokerCardSideText isInverted={true} />
+            <JokerCardSideText
+              isInverted={true}
+              isDisabled={jokerInfo.jokerTextDisabled}
+            />
           </div>
         </div>
 
         <div className="flex flex-col items-center bg-[#3f4a4d] w-full rounded py-3 px-2 pixel-corners white">
-          <span className="text-white text-5xl">{name}</span>
+          <span className="text-white text-5xl">
+            {jokerInfo.name != "" ? jokerInfo.name : "Joker"}
+          </span>
           <div className="bg-white pixel-corners rounded text-[#3d5458] w-full text-center text-3xl">
             {/* <span className="rounded pl-1 pt-1 bg-red-500 text-white">X3</span> */}
-            {desc}
+            {jokerInfo.desc}
             {/* Mult
             <br />
             Destroy this if played
@@ -30,7 +46,7 @@ export default function Joker({ isBig, name, desc, rarity, overlay, mainImage, b
           <div className="pixel-corners w-3/4">
             <div className="bg-[#34bc85] pixel-corners px-3 rounded text-center mt-2">
               <span className="text-white text-4xl text-shadow-lg">
-                {rarity}
+                {jokerInfo.rarity}
               </span>
             </div>
           </div>
@@ -42,33 +58,37 @@ export default function Joker({ isBig, name, desc, rarity, overlay, mainImage, b
   }
 }
 
-function JokerCardSideText({ isInverted }) {
+function JokerCardSideText({ isInverted, isDisabled }) {
   if (isInverted) {
     return (
       <div
         className="flex flex-col items-start"
         style={{ transform: "rotate(180deg) scaleX(-1)" }}
       >
-        <JokerText />
+        <JokerText isDisabled={isDisabled} />
       </div>
     );
   } else {
     return (
       <div className="flex flex-col items-start">
-        <JokerText />
+        <JokerText isDisabled={isDisabled} />
       </div>
     );
   }
 }
 
-function JokerText() {
-  return (
-    <>
-      <span className="joker-text">J</span>
-      <span className="joker-text">O</span>
-      <span className="joker-text">K</span>
-      <span className="joker-text">E</span>
-      <span className="joker-text">R</span>
-    </>
-  );
+function JokerText({ isDisabled }) {
+  if (isDisabled) {
+    return <span className="joker-text !text-transparent">K</span>;
+  } else {
+    return (
+      <>
+        <span className="joker-text">J</span>
+        <span className="joker-text">O</span>
+        <span className="joker-text">K</span>
+        <span className="joker-text">E</span>
+        <span className="joker-text">R</span>
+      </>
+    );
+  }
 }
